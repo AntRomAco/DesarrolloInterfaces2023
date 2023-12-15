9078c1b3636b8b3a09e3a3afe0339399362f67cb
AntRomAco <antonioromeroacosta@outlook.es>
2023-12-15 17:28:47 +0100

    gildedrose

--- GildedRose/GildedRose.cs
+++ GildedRose/GildedRose.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Engine;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace csharp
 {
@@ -12,15 +11,135 @@ namespace csharp
             this.Items = Items;
         }
 
-        private void DecreaseQuality(Item item, int factor = 1)
+        public void UpdateQuality()
         {
-            if (item.Quality > 0)
+            foreach (var item in Items)
             {
-                item.Quality -= factor;
+                ItemUpdater updater = GetUpdater(item);
+                updater.UpdateQuality(item);
             }
         }
 
-        private void IncreaseQuality(Item item, int factor = 1)
+        private ItemUpdater GetUpdater(Item item)
+        {
+            switch (item.Name)
+            {
+                case "+5 Dexterity Vest":
+                    return new DexterityVestUpdater();
+                case "Aged Brie":
+                    return new AgedBrieUpdater();
+                case "Elixir of the Mongoose":
+                    return new ElixirUpdater();
+                case "Sulfuras, Hand of Ragnaros":
+                    return new SulfurasUpdater();
+                case "Backstage passes to a TAFKAL80ETC concert":
+                    return new BackstagePassUpdater();
+                case "Conjured Mana Cake":
+                    return new ConjuredManaCakeUpdater();
+                default:
+                    return new DefaultItemUpdater();
+            }
+        }
+    }
+
+    public class DexterityVestUpdater : ItemUpdater
+    {
+        public override void UpdateQuality(Item item)
+        {
+            DecreaseQuality(item);
+            DecreaseSellIn(item);
+
+            if (item.SellIn < 0)
+            {
+                DecreaseQuality(item);
+            }
+        }
+    }
+
+    public class AgedBrieUpdater : ItemUpdater
+    {
+        public override void UpdateQuality(Item item)
+        {
+            IncreaseQuality(item);
+            DecreaseSellIn(item);
+
+            if (item.SellIn < 0)
+            {
+                IncreaseQuality(item);
+            }
+        }
+    }
+
+    public class ElixirUpdater : ItemUpdater
+    {
+        public override void UpdateQuality(Item item)
+        {
+            DecreaseQuality(item);
+            DecreaseSellIn(item);
+
+            if (item.SellIn < 0)
+            {
+                DecreaseQuality(item);
+            }
+        }
+    }
+
+    public class SulfurasUpdater : ItemUpdater
+    {
+        public override void UpdateQuality(Item item) { }
+    }
+
+    public class BackstagePassUpdater : ItemUpdater
+    {
+        public override void UpdateQuality(Item item)
+        {
+            IncreaseQuality(item);
+
+            if (item.SellIn <= 10)
+            {
+                IncreaseQuality(item);
+            }
+
+            if (item.SellIn <= 5)
+            {
+                IncreaseQuality(item);
+            }
+
+            DecreaseSellIn(item);
+
+            if (item.SellIn < 0)
+            {
+                item.Quality = 0;
+            }
+        }
+    }
+
+    public class ConjuredManaCakeUpdater : ItemUpdater
+    {
+        public override void UpdateQuality(Item item)
+        {
+            DecreaseQuality(item, 2);
+            DecreaseSellIn(item);
+        }
+    }
+
+    public class DefaultItemUpdater : ItemUpdater
+    {
+        public override void UpdateQuality(Item item)
+        {
+            DecreaseQuality(item);
+            DecreaseSellIn(item);
+
+            if (item.SellIn < 0)
+            {
+                DecreaseQuality(item);
+            }
+        }
+    }
+
+    public abstract class ItemUpdater
+    {
+        protected void IncreaseQuality(Item item, int factor = 1)
         {
             if (item.Quality < 50)
             {
@@ -33,66 +152,27 @@ namespace csharp
             }
         }
 
-        public void UpdateQuality()
+        protected void DecreaseQuality(Item item, int factor = 1)
         {
-            foreach (var item in Items)
+            if (item.Quality > 0)
             {
-                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    item.SellIn -= 1;
-                }
+                item.Quality -= factor;
 
-                switch (item.Name)
+                if (item.Quality < 0)
                 {
-                    case "Aged Brie":
-                        IncreaseQuality(item);
-                        break;
-                    case "Backstage passes to a TAFKAL80ETC concert":
-                        if (item.SellIn < 0)
-                        {
-                            item.Quality = 0;
-                        }
-                        else if (item.SellIn < 5)
-                        {
-                            IncreaseQuality(item, 3);
-                        }
-                        else if (item.SellIn < 10)
-                        {
-                            IncreaseQuality(item, 2);
-                        }
-                        else
-                        {
-                            IncreaseQuality(item);
-                        }
-                        break;
-                    case "Sulfuras, Hand of Ragnaros":
-                        break;
-                    case "Conjured Mana Cake":
-                        DecreaseQuality(item, 2);
-                        break;
-                    default:
-                        DecreaseQuality(item);
-                        break;
-                }
-
-                if (item.SellIn < 0)
-                {
-                    switch (item.Name)
-                    {
-                        case "Aged Brie":
-                            IncreaseQuality(item);
-                            break;
-                        case "Backstage passes to a TAFKAL80ETC concert":
-                            item.Quality = 0;
-                            break;
-                        case "Sulfuras, Hand of Ragnaros":
-                            break;
-                        default:
-                            DecreaseQuality(item);
-                            break;
-                    }
+                    item.Quality = 0;
                 }
             }
         }
+
+        protected void DecreaseSellIn(Item item)
+        {
+            if (item.Name != "Sulfuras, Hand of Ragnaros")
+            {
+                item.SellIn -= 1;
+            }
+        }
+
+        public abstract void UpdateQuality(Item item);
     }
 }
